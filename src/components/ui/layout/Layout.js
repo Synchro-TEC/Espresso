bc5b932a72ba5d2ead1c3b13ee5b77546a7f41fa
Marcio Gasparotto <mgasparotto@gmail.com>
2017-08-09 13:15:21 +0000

    menu dinamico

--- src/components/ui/layout/Layout.js
+++ src/components/ui/layout/Layout.js
@@ -19,42 +19,60 @@ class Layout extends Component {
     );
   }
 
+  renderGuideMenu() {
+    return (
+      <ul>
+        <li>
+          <NavLink activeClassName="selected" exact to="/guide">
+            Introdução
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="selected" exact to="/guide/espacamentos">
+            Espaçamentos
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="selected" exact to="/guide/alinhamentos">
+            Alinhamento
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="selected" exact to="/guide/contraste">
+            Contraste
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="selected" exact to="/guide/sejadireto">
+            Seja Direto
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="selected" exact to="/guide/synchro">
+            Synchro Headers
+          </NavLink>
+        </li>
+      </ul>
+    );
+  }
+
+  renderComponentsMenu() {
+    return (
+      <ul>
+        <li>
+          <NavLink activeClassName="selected" exact to="/components">
+            Component
+          </NavLink>
+        </li>
+      </ul>
+    );
+  }
+
   renderPage() {
     return (
       <div className="content">
         <aside className="inner-menu sv-pv--20">
-          <ul>
-            <li>
-              <NavLink activeClassName="selected" exact to="/guide">
-                Introdução
-              </NavLink>
-            </li>
-            <li>
-              <NavLink activeClassName="selected" exact to="/guide/espacamentos">
-                Espaçamentos
-              </NavLink>
-            </li>
-            <li>
-              <NavLink activeClassName="selected" exact to="/guide/alinhamentos">
-                Alinhamento
-              </NavLink>
-            </li>
-            <li>
-              <NavLink activeClassName="selected" exact to="/guide/contraste">
-                Contraste
-              </NavLink>
-            </li>
-            <li>
-              <NavLink activeClassName="selected" exact to="/guide/sejadireto">
-                Seja Direto
-              </NavLink>
-            </li>
-            <li>
-              <NavLink activeClassName="selected" exact to="/guide/synchro">
-                Synchro Headers
-              </NavLink>
-            </li>
-          </ul>
+          {this.props.match.path.includes('/guide') ? this.renderGuideMenu() : this.renderComponentsMenu()}
         </aside>
         <section className="inner-content sv-pa--20">
           {this.props.children}
