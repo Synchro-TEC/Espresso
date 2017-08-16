1d53eac3ebdf2df4c5e33ff7b12517802f09bb00
Marcio Gasparotto <mgasparotto@gmail.com>
2017-08-16 17:04:35 -0300

    saturn-v address

--- src/codes/install.js
+++ src/codes/install.js
@@ -1,6 +1,6 @@
 export default {
   installSaturn: `<link rel="stylesheet" href="https://cdn.synchro.com.br/assets/fontawesome/4.7.0/css/font-awesome.css" />
-<link rel="stylesheet" href="https://cdn.synchro.com.br/assets/saturn-v/beta-3/saturn-v.css" />`,
+<link rel="stylesheet" href="https://cdn.synchro.com.br/assets/saturn-v/rc/saturn-v.css" />`,
   installApollo: `# Usando NPM
 npn install --save syntec-apollo-11
 # Usando Yarn
