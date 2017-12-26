5ce6a446f518c5b9e4b2949a4e5c295e05f39cea
Mateus Marquezini <mateusmarquezini01@gmail.com>
2017-12-26 16:22:35 -0200

    Melhorias na tela de contribuição do Espresso. Foram adicionados alguns novos ícones, isolamento de alguns contextos de instruções e correções de gramática

--- src/pages/components/contribuir.js
+++ src/pages/components/contribuir.js
@@ -7,41 +7,59 @@ class Contribuir extends Component {
       <div>
         <h3 className="sv-fw-normal sv-mb--25">COMO CONTRIBUIR</h3>
         <p>
-          Existe várias maneiras de contribuir com o desenvolvimento do Espresso, seja com ideias, testando e reportando
-          possíveis problemas ou ainda ajudando na codificação, com correção de problemas e implementaçã de novas
+          Existem várias maneiras de contribuir com o desenvolvimento do Espresso, seja ela com ideias; testando;
+          reportando erros ou problemas; ajudando na codificação com correções de bugs e implementação de novas
           funcionalidades.
         </p>
         <p>
-          Toda interação deve ser feita através do sistema de issues, desde bugs até debates sobre novas funcionalidades
-          devem ser centralizados nas issues de cada projeto.
+          Toda interação deve ser feita e centralizada através do sistema de issues de cada projeto. Desde bugs, até
+          debates sobre novas funcionalidades.
         </p>
         <p>Lembramos que o Espresso é composto por 3 projetos:</p>
         <ul>
           <li>
             Saturn-V - Nosso Framework CSS{' '}
-            <a href="https://github.com/Synchro-TEC/saturn-v">[https://github.com/Synchro-TEC/saturn-v]</a>
+            <a href="https://github.com/Synchro-TEC/saturn-v">
+              <i className="fa fa-external-link sv-pl--5" aria-hidden="true" />
+            </a>
           </li>
           <li>
             Apollo-11 - Set de componentes React{' '}
-            <a href="https://github.com/Synchro-TEC/apollo-11">[https://github.com/Synchro-TEC/apollo-11]</a>
+            <a href="https://github.com/Synchro-TEC/apollo-11">
+              <i className="fa fa-external-link sv-pl--5" aria-hidden="true" />
+            </a>
           </li>
           <li>
             Site de documentação do Espresso - Este que você está lendo agora!{' '}
-            <a href="https://github.com/Synchro-TEC/Espresso">[https://github.com/Synchro-TEC/Espresso]</a>
+            <a href="https://github.com/Synchro-TEC/Espresso">
+              <i className="fa fa-external-link sv-pl--5" aria-hidden="true" />
+            </a>
           </li>
         </ul>
-        <h4 className="sv-fw-normal sv-ts-i sv-mt--40">Contribuindo com o Saturn-V</h4>
+        <h4 className="sv-fw-normal sv-ts-i sv-mt--40">
+          <i className="fa fa-code-fork sv-pr--5" aria-hidden="true" />
+          Contribuindo com o Saturn-V
+        </h4>
         <hr />
         <p>
-          Para abertura de issues:{' '}
-          <a href="https://github.com/Synchro-TEC/saturn-v/issues">https://github.com/Synchro-TEC/saturn-v/issues</a>
+          Para abertura de issues, acesse nossa{' '}
+          <a href="https://github.com/Synchro-TEC/saturn-v/issues">
+            área
+            <i className="fa fa-external-link sv-pl--5" aria-hidden="true" />
+          </a>
         </p>
-        <p>Se você deseja nos ajudar codificando, o processo também é bem simples:</p>
+        <p>Se você deseja nos ajudar no desenvolvimento, o processo também é bem simples:</p>
         <ol className="sv-ol">
-          <li>Faça um fork do projeto</li>
           <li>
-            Faça um clone do seu fork e instale as dependências, basta rodar na raiz:{' '}
-            <code className="sv-code">npm install</code>
+            Faça o fork do projeto clicando {''}
+            <a href="https://github.com/Synchro-TEC/saturn-v">
+              aqui <i className="fa fa-external-link fa-fw" />
+            </a>
+          </li>
+          <li>
+            Após a primeira etapa, faça um clone do seu fork em seu ambiente de trabalho e dentro da raiz do projeto,
+            instale as dependências rodando o comando <code className="sv-code">npm install</code> no terminal do seu
+            sistema operacional.
           </li>
           <li>
             Instale a dependência para gerar a build do projeto:{' '}
@@ -52,22 +70,16 @@ class Contribuir extends Component {
             diretório, para que você possa testar sua implementação
           </li>
           <li>
-            Para rodar a build do projeto em desenvolvimento: <code className="sv-code">grunt watch</code>
+            Para rodar a build do projeto em desenvolvimento, execute o comando:{' '}
+            <code className="sv-code">grunt watch</code>
           </li>
-          <li>Ao finalizar sua implementação submeta um pull request no github!</li>
-          <li>IMPORTANTE: mantenha o seu fork atualizado com repositório principal.</li>
-          <li>
-            <code className="sv-code">git remote add upstream git@github.com:Synchro-TEC/saturn-v.git</code>
-          </li>
-          <li>
-            <code className="sv-code">git fetch upstream</code>
-          </li>
-          <li>
-            <code className="sv-code">git pull upstream master</code> (ou qualquer outra branch que desejar).
-          </li>
+          <li>Ao finalizar sua implementação submeta um pull request no GitHub!</li>
         </ol>
 
-        <h4 className="sv-fw-normal sv-ts-i sv-mt--40">Contribuindo com o Apollo-11</h4>
+        <h4 className="sv-fw-normal sv-ts-i sv-mt--40">
+          <i className="fa fa-code-fork sv-pr--5" aria-hidden="true" />
+          Contribuindo com o Apollo-11
+        </h4>
         <hr />
         <p>
           Para abertura de issues:{' '}
@@ -95,19 +107,12 @@ class Contribuir extends Component {
             Para rodar a build do projeto em desenvolvimento: <code className="sv-code">yarn watch</code>
           </li>
           <li>Ao finalizar sua implementação submeta um pull request no github!</li>
-          <li>IMPORTANTE: mantenha o seu fork atualizado com repositório principal.</li>
-          <li>
-            <code className="sv-code">git remote add upstream git@github.com:Synchro-TEC/apollo-11.git</code>
-          </li>
-          <li>
-            <code className="sv-code">git fetch upstream</code>
-          </li>
-          <li>
-            <code className="sv-code">git pull upstream master</code> (ou qualquer outra branch que desejar).
-          </li>
         </ol>
 
-        <h4 className="sv-fw-normal sv-ts-i sv-mt--40">Contribuindo com a documentação do Espresso</h4>
+        <h4 className="sv-fw-normal sv-ts-i sv-mt--40">
+          <i className="fa fa-code-fork sv-pr--5" aria-hidden="true" />
+          Contribuindo com a documentação do Espresso
+        </h4>
         <hr />
         <p>
           Para abertura de issues:{' '}
@@ -128,15 +133,26 @@ class Contribuir extends Component {
             Para rodar a build do projeto em desenvolvimento: <code className="sv-code">yarn start</code>
           </li>
           <li>Ao finalizar sua implementação submeta um pull request no github!</li>
-          <li>IMPORTANTE: mantenha o seu fork atualizado com repositório principal.</li>
+        </ol>
+        <h4 className="sv-fw-normal sv-ts-i sv-mt--40">
+          <i className="fa fa-lightbulb-o sv-pr--5" aria-hidden="true" />
+          Dica: Mantendo seu fork atualizado com o repositório central
+        </h4>
+        <hr />
+        <p>
+          Para manter seu fork atualizado com as mais recentes mudanças do repositório central, basta executar os
+          comandos a seguir, por etapa via terminal na raiz do projeto.
+        </p>
+        <ol className="sv-ol">
           <li>
-            <code className="sv-code">git remote add upstream git@github.com:Synchro-TEC/Espresso.git</code>
+            <code className="sv-code">git remote add upstream git@github.com:NOME_REPOSITORIO</code> ex:
+            <span className="sv-ts-i"> git@github.com:Synchro-TEC/Espresso.git</span>
           </li>
           <li>
             <code className="sv-code">git fetch upstream</code>
           </li>
           <li>
-            <code className="sv-code">git pull upstream master</code> (ou qualquer outra branch que desejar).
+            <code className="sv-code">git pull upstream NOME_BRANCH</code> ex: <span className="sv-ts-i">master</span>
           </li>
         </ol>
       </div>
