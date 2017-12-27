b260fb41f938ffb7909985677141fac7498ae1e7
Mateus Marquezini <mateusmarquezini01@gmail.com>
2017-12-27 14:15:16 -0200

    Melhorias na página de contribuição do Espresso

--- src/pages/components/contribuir.js
+++ src/pages/components/contribuir.js
@@ -18,42 +18,98 @@ class Contribuir extends Component {
         <p>Lembramos que o Espresso é composto por 3 projetos:</p>
         <ul>
           <li>
-            Saturn-V - Nosso Framework CSS{' '}
+            <span className="sv-fw-bold">Saturn-V</span> - Framework CSS{' '}
             <a href="https://github.com/Synchro-TEC/saturn-v">
               <i className="fa fa-external-link sv-pl--5" aria-hidden="true" />
             </a>
           </li>
           <li>
-            Apollo-11 - Set de componentes React{' '}
+            <span className="sv-fw-bold">Apollo-11</span> - Componentes React{' '}
             <a href="https://github.com/Synchro-TEC/apollo-11">
               <i className="fa fa-external-link sv-pl--5" aria-hidden="true" />
             </a>
           </li>
           <li>
-            Site de documentação do Espresso - Este que você está lendo agora!{' '}
+            <span className="sv-fw-bold">Espresso</span> - Site de documentação. Este mesmo que você está lendo agora!{' '}
             <a href="https://github.com/Synchro-TEC/Espresso">
               <i className="fa fa-external-link sv-pl--5" aria-hidden="true" />
             </a>
           </li>
         </ul>
+        <br />
+        <p>
+          <span className="sv-ts-i sv-ts-u">
+            Ao finalizar sua implementação, não se hesite em nos submeter um pull request!{' '}
+          </span>
+          <i className="fa fa-thumbs-o-up sv-pl--5" aria-hidden="true" />
+        </p>
+        <h4 className="sv-fw-normal sv-ts-i sv-mt--40">
+          <i className="fa fa-check-square-o sv-pr--5" aria-hidden="true" />
+          Requisitos
+        </h4>
+        <hr />
+        <p>
+          Para contribuir com nossos projetos, é necessário alguns requisitos mínimos em seu ambiente de trabalho, entre
+          eles:
+        </p>
+        <h5>Node.js & npm</h5>
+        <p>
+          Para se trabalhar com nossos projetos, é indispensável que você possua instalado o Node.js e o NPM em seu
+          ambiente de trabalho. Acesso o{' '}
+          <a href="https://nodejs.org/en/">
+            site do Node.js
+            <i className="fa fa-external-link sv-pl--5" aria-hidden="true" />
+          </a>{' '}
+          e siga as instruções para instalação. Lembrando que o npm é distribuído com o Node.js. Isso significa que
+          quando você baixa e instala o Node.js, automáticamente você irá ter o npm instalado em sua máquina.
+        </p>
+        <p>
+          Certifique-se de que a instalação do Node.js foi realizada com sucesso executando o comando abaixo em seu
+          terminal:
+        </p>
+        <p>
+          <code className="sv-code">node -v</code>
+        </p>
+        <p>Para confirmar que você instalou o npm com sucesso, execute o comando abaixo em seu terminal:</p>
+        <p>
+          <code className="sv-code">npm -v</code>
+        </p>
+        <h5>Grunt</h5>
+        <p>
+          Para instalar, abra um terminal e rode o comando <code className="sv-code"> npm install -g grunt-cli</code>
+        </p>
+        <h5>Yarn</h5>
+        <p>
+          Para instalar o gerenciador de dependência, acesse o{' '}
+          <a href="https://yarnpkg.com/en/docs/install">
+            site do Yarn
+            <i className="fa fa-external-link sv-pl--5" aria-hidden="true" />
+          </a>{' '}
+          e siga as instruções para instalação.
+        </p>
+        <p>
+          Certifique-se de que a instalação do Yarn foi realizada com sucesso executando o comando abaixo em seu
+          terminal:
+        </p>
+        <p>
+          <code className="sv-code">yarn -v</code>
+        </p>
         <h4 className="sv-fw-normal sv-ts-i sv-mt--40">
           <i className="fa fa-code-fork sv-pr--5" aria-hidden="true" />
           Contribuindo com o Saturn-V
         </h4>
         <hr />
-        <p>
-          Para abertura de issues, acesse nossa{' '}
-          <a href="https://github.com/Synchro-TEC/saturn-v/issues">
-            área
-            <i className="fa fa-external-link sv-pl--5" aria-hidden="true" />
-          </a>
-        </p>
-        <p>Se você deseja nos ajudar no desenvolvimento, o processo também é bem simples:</p>
+        Para abertura de issues, acesse {' '}
+        <a href="https://github.com/Synchro-TEC/saturn-v/issues">
+          aqui
+          <i className="fa fa-external-link sv-pl--5" aria-hidden="true" />
+        </a>
+        <p>Se você deseja nos ajudar no desenvolvimento e crescimento do Saturn-V, o processo é bem simples:</p>
         <ol className="sv-ol">
           <li>
-            Faça o fork do projeto clicando {''}
+            Faça o fork do projeto acessando o {''}
             <a href="https://github.com/Synchro-TEC/saturn-v">
-              aqui <i className="fa fa-external-link fa-fw" />
+              repositório <i className="fa fa-external-link fa-fw" />
             </a>
           </li>
           <li>
@@ -62,77 +118,75 @@ class Contribuir extends Component {
             sistema operacional.
           </li>
           <li>
-            Instale a dependência para gerar a build do projeto:{' '}
-            <code className="sv-code">npm install -g grunt-cli</code>
-          </li>
-          <li>
             Altere o arquivo <code className="sv-code">grunt/exec.js</code> para gerar uma cópia em um determinado
             diretório, para que você possa testar sua implementação
           </li>
           <li>
-            Para rodar a build do projeto em desenvolvimento, execute o comando:{' '}
-            <code className="sv-code">grunt watch</code>
+            Para executar a build do projeto em desenvolvimento, na raiz do projeto, execute o comando
+            <code className="sv-code">grunt watch</code> em um terminal.
           </li>
-          <li>Ao finalizar sua implementação submeta um pull request no GitHub!</li>
         </ol>
-
         <h4 className="sv-fw-normal sv-ts-i sv-mt--40">
           <i className="fa fa-code-fork sv-pr--5" aria-hidden="true" />
           Contribuindo com o Apollo-11
         </h4>
         <hr />
-        <p>
-          Para abertura de issues:{' '}
-          <a href="https://github.com/Synchro-TEC/apollo-11/issues">https://github.com/Synchro-TEC/apollo-11/issues</a>
-        </p>
-        <p>Para desenvolvedores:</p>
+        Para abertura de issues, acesse{' '}
+        <a href="https://github.com/Synchro-TEC/apollo-11/issues">
+          aqui <i className="fa fa-external-link fa-fw" />
+        </a>
+        <p>Se você deseja nos ajudar no desenvolvimento e crescimento do Apollo-11, o processo também é bem simples:</p>
         <ol className="sv-ol">
-          <li>Faça um fork do projeto</li>
           <li>
-            Instale o yarn para gerencimanto de dependências:{' '}
-            <a href="https://yarnpkg.com/en/docs/install">https://yarnpkg.com/en/docs/install</a>
+            Faça um fork do projeto acessando o {' '}
+            <a href="https://github.com/Synchro-TEC/apollo-11">
+              repositório<i className="fa fa-external-link fa-fw sv-pl--5" />
+            </a>
           </li>
           <li>
-            Faça um clone do seu fork e instale as dependências, basta rodar na raiz:{' '}
-            <code className="sv-code">yarn</code>
+            Após a primeira etapa, faça um clone do seu fork em seu ambiente de trabalho e dentro da raiz do projeto,
+            instale as dependências rodando o comando <code className="sv-code">yarn</code> no terminal do seu sistema
+            operacional.
           </li>
           <li>
-            Para guiar a sua implementação e poder testar no "mundo real" você precisa de um aprojeto rodando o
-            Apollo-11, caso já possua ele como dependência do seu projeto remova e tenha certeza de apagar o diretório
-            do mesmo na pasta node_modules. Feito isso rode o comando na raiz do seu fork:{' '}
-            <code className="sv-code">yarn link</code>, depois no projet oque vai usar ele para testar rode:
-            <code className="sv-code">yarn link synctec-apollo-11</code>.
+            Para guiar a sua implementação ao decorrer do desenvolvimento e poder testar, você precisa de um projeto
+            rodando o Apollo-11, caso já possua ele como dependência de um outro projeto, remova-o deletando o diretório
+            na pasta <span className="sv-fw-bold">node_modules</span> do seu projeto. Feito isso, na raiz do seu fork
+            execute <code className="sv-code">yarn link</code>, depois no projeto que vai usar o Apollo-11 para testar
+            sua implementação, rode o comando
+            <code className="sv-code">yarn link syntec-apollo-11</code> em seu terminal.
           </li>
           <li>
-            Para rodar a build do projeto em desenvolvimento: <code className="sv-code">yarn watch</code>
+            Para executar a build do projeto em desenvolvimento, na raiz do projeto, execute o comando{' '}
+            <code className="sv-code">yarn watch</code> em seu terminal.
           </li>
-          <li>Ao finalizar sua implementação submeta um pull request no github!</li>
         </ol>
-
         <h4 className="sv-fw-normal sv-ts-i sv-mt--40">
           <i className="fa fa-code-fork sv-pr--5" aria-hidden="true" />
           Contribuindo com a documentação do Espresso
         </h4>
         <hr />
-        <p>
-          Para abertura de issues:{' '}
-          <a href="https://github.com/Synchro-TEC/Espresso/issues">https://github.com/Synchro-TEC/Espresso/issues</a>
-        </p>
-        <p>Para desenvolvedores:</p>
+        Para abertura de issues, acesse{' '}
+        <a href="https://github.com/Synchro-TEC/Espresso/issues">
+          aqui <i className="fa fa-external-link fa-fw" />
+        </a>
+        <p>Se você deseja nos ajudar no desenvolvimento e crescimento do Espresso, o processo também é bem simples:</p>
         <ol className="sv-ol">
-          <li>Faça um fork do projeto</li>
           <li>
-            Instale o yarn para gerencimanto de dependências:{' '}
-            <a href="https://yarnpkg.com/en/docs/install">https://yarnpkg.com/en/docs/install</a>
+            Faça um fork do projeto acessando o {' '}
+            <a href="https://github.com/Synchro-TEC/Espresso">
+              repositório<i className="fa fa-external-link fa-fw sv-pl--5" />
+            </a>
           </li>
           <li>
-            Faça um clone do seu fork e instale as dependências, basta rodar na raiz:{' '}
-            <code className="sv-code">yarn</code>
+            Após a primeira etapa, faça um clone do seu fork em seu ambiente de trabalho e dentro da raiz do projeto,
+            instale as dependências rodando o comando <code className="sv-code">yarn</code> no terminal do seu sistema
+            operacional.
           </li>
           <li>
-            Para rodar a build do projeto em desenvolvimento: <code className="sv-code">yarn start</code>
+            Para executar a build do projeto em desenvolvimento, na raiz do projeto, execute o comando{' '}
+            <code className="sv-code">yarn start</code> em seu terminal.
           </li>
-          <li>Ao finalizar sua implementação submeta um pull request no github!</li>
         </ol>
         <h4 className="sv-fw-normal sv-ts-i sv-mt--40">
           <i className="fa fa-lightbulb-o sv-pr--5" aria-hidden="true" />
